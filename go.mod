a20b704d3046e6cc2e7e171ba4d4c80ca8b7f865
Andras Acs <andras.acs@bitrise.io>
2020-12-18 10:00:05 +0100

    Working SSO poc, but with sdtlib client

---
+++ go.mod
@@ -0,0 +1,5 @@
+module sso-poc
+
+go 1.14
+
+require github.com/bitrise-io/bitrise-oauth v0.0.0-20201130133827-7518e82af50e
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bitrise-io/bitrise-oauth/client"
+	"github.com/bitrise-io/bitrise-oauth/config"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmt.Print("Hello from my auth poc!")
+	var realmOption client.Option = client.WithRealm("anka")
+	baseUrlOption := client.WithBaseURL("http://192.168.1.3:8080")
+	scope := client.WithScope("groups")
+	authProvider := client.NewWithSecret(
+		"anka-controller",
+		"712e1f52-4986-4933-861b-5d5cc4ed9bc4",
+		scope,
+		baseUrlOption,
+		realmOption)
+
+	tokenSource := authProvider.UMATokenSource()
+	token, err := tokenSource.Token(
+		nil,
+		make([]client.Permission, 0),
+		config.NewAudienceConfig("anka-controller"))
+
+	if err != nil{
+		fmt.Println()
+		fmt.Printf("Could not get a token! Error: %q\n", err)
+		os.Exit(1)
+	}
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("AccessToken: %s\n", token.AccessToken)
+	//httpClient := authProvider.HTTPClient()
+	httpClient := &http.Client{}
+
+	req, _ := http.NewRequest("GET", "http://192.168.1.110/api/v1/vm", nil)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		fmt.Printf("\n\n\nError happened during the request: %s", err)
+	}
+	fmt.Printf("\n\n\nRequest: %+v.\n\nHeaders: %+v, (Count: %d)",
+		req,
+		req.Header,
+		len(req.Header))
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	bodyString := string(bodyBytes)
+	fmt.Printf("\n\n\nResponse: %+v. \n\nBody: %q", resp, bodyString)
+}
